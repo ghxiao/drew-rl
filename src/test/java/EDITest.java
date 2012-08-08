00e7699c0ffab91e1cb9fa2d488ec95cc3df07a1
ghxiao <gh.xiao@github.com>
2012-08-08 21:18:07 +0200

    BI test

---
+++ src/test/java/EDITest.java
@@ -0,0 +1,47 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+import java.util.Set;
+
+import org.semanticweb.drew.dlprogram.Clause;
+import org.semanticweb.drew.ldlp.reasoner.LDLPOntologyCompiler;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+
+public class EDITest {
+	public static void main(String[] args) throws URISyntaxException, OWLOntologyCreationException, IOException {
+		
+		URL url = EDITest.class.getResource("/MessageBI.owl");
+		IRI iri = IRI.create(url);
+		
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		OWLOntology ontology = manager.loadOntologyFromOntologyDocument(iri);
+		
+		
+		LDLPOntologyCompiler compiler = new LDLPOntologyCompiler();
+		System.out.println("Ontology:");
+		Set<OWLAxiom> axioms = ontology.getAxioms();
+		
+
+		System.out.println("-------------------------------------------------------");
+
+		List<Clause> clauses = compiler.compile(axioms);
+		
+		FileWriter writer = new FileWriter("./edi.dlv");
+		
+		System.out.println("Compiled Ontoloy:");
+		for (Clause clause : clauses) {
+			//writer.write(clause);
+			writer.write(clause.toString());
+			writer.write("\n");			
+		}
+		writer.close();
+		
+	}
+}
